eb5e68370441354498c5a26f6a1d35504b8cce1d
Golden-Serpents <71458324+Golden-Serpents@users.noreply.github.com>
2021-08-18 22:59:30 +0100

    addition of comments
    added comments and removed code that was a bunch of sh*t from average

--- average.py
+++ average.py
@@ -4,35 +4,21 @@ true_total_wishes = 0
 price_of_wishes = 2
 
 
-with open("number_of_wishes_experimental.txt", mode = "r", encoding = "UTF-8") as file:
+with open("number_of_wishes_experimental.txt", mode = "r", encoding = "UTF-8") as file:         ##reading the file into a variable then splitting into a list
     content = file.read()
     wish_list = content.split("\n")
     
-recursion = int(len(wish_list) - 1)
+recursion = int(len(wish_list) - 1)         ##last line is always '' hence last item in the list therefore can't be added to the list, this is my lazy way around it
 
-for i in range(0, recursion):
-    if int(wish_list[int(i)]) > 1:
-        total_wishes += int(wish_list[int(i)])
-        true_total_wishes += int(wish_list[int(i)])
-        recursion_reduction += 1
-        
-    else:
-        true_total_wishes += int(wish_list[int(i)])
+for i in range(0, recursion):       ##totalling up the number of wishes used
 
-adjusted_recursion = recursion - recursion_reduction
+    total_wishes += int(wish_list[int(i)])
 
-print("this is how many total wishes were required if you never hit first time " + str(total_wishes))
+
+print("this is how many total wishes were required " + str(total_wishes))       ##printing the total wishes and the amount of times the character was obtained
 print("to get " + str(recursion) + " of the desired 5* character")
-average = total_wishes/recursion
-cost = price_of_wishes*average
+average = total_wishes/recursion            ##finding the average
+cost = price_of_wishes*average              ##finding the average cost of the character
 
-print("this is how many wishes it took on average: " + str(average) + "\n")
-print("this would cost $" + str())
-
-print("this is how many total wishes were required " + str(true_total_wishes))
-print("to get " + str(recursion) + " of the desired 5* character\n")
-average = true_total_wishes/adjusted_recursion
-cost = price_of_wishes*average
-
-print("this is how many wishes it took on average: " + str(average))
+print("this is how many wishes it took on average: " + str(average) + "\n")     ##printing the average and then the cost
 print("this would cost $" + str())
--- wish_simulator.py
+++ wish_simulator.py
@@ -1,13 +1,11 @@
 import random
 
-exit_loop = False
-
 number_of_wishes = 0
 wish = []
 desired = [0, 1]
 guarantee = False
 
-for i in range(0,854):
+for i in range(0,854):  ##creating the list of values with some duplicates to create weighted probability
     wish.append(i)
     
 for i in range(16):
@@ -16,58 +14,60 @@ for i in range(16):
 for i in range(130):
     wish.append(1000)
 
-for i in range(0, 100):
+for i in range(0, 100):       ##looping for a set number of wishes
     
-    result = random.choice(wish)
+    result = random.choice(wish)        ##chosing result of the wish
     
-    if number_of_wishes % 90 == 0 and number_of_wishes > 0 and guarantee == False:
+    if number_of_wishes % 90 == 0 and number_of_wishes > 0:     ##creating an approximation of the pitty system, only approximating as i don't want to
         result = 2000
 
-    elif number_of_wishes % 10 == 0 and number_of_wishes > 0:
+    elif number_of_wishes % 10 == 0 and number_of_wishes > 0:   ##try to go from 0.6% through to 100% at 90 wishes and just use 1.6% as average with a guarantee every 90th
         result = 1000
         
-    else:
+    else:           ##finishing the selection statement
         result = result
         
-    if guarantee == False:
+    if guarantee == False:      ##proceedure when a 4/5 star that is not part of the set of featured characters hasn't been rolled yet
         
-        featured = random.choice(desired)
+        featured = random.choice(desired) ##chosing whether the character is desired if it is 4/5 star if it is rolled, it may significantly reduce computing time if it is only done when the result is rolled however i would have to rewrite code and i'm lazy with plenty of time
         
-        if result == 1000 and featured == 1:
+        if result == 1000 and featured == 1:        ##a 4* being rolled that is in the set of featured characters
             number_of_wishes += 1
 
-        elif result == 1000 and featured == 0:
+        elif result == 1000 and featured == 0:      ##a 4* being rolled that is not in the set of featured characters granting a guarantee that the next 4/5 star rolled is in the set of featured characters
             guarantee = True
             number_of_wishes += 1
 
-        elif result == 2000 and featured == 1:
+        elif result == 2000 and featured == 1:      ##a 5* being rolled in the set of featured characters
             number_of_wishes += 1
-            exit_loop = True
 
-            with open("number_of_wishes_experimental.txt", mode = "a", encoding = "UTF-8") as file:
+            with open("number_of_wishes_experimental.txt", mode = "a", encoding = "UTF-8") as file: ##appending the number of wishes taken to roll the desired 5 star to the file
                 file.write(str(number_of_wishes) + "\n")
 
-            number_of_wishes = 0
+            number_of_wishes = 0        ##resetting number of wishes so that it doesn't become a cumulative total of wishes
             
-        elif result == 2000 and featured == 0:
+        elif result == 2000 and featured == 0:  ##a 5* being rolled that is not in the set of featured characters creating a guarantee that the next 4/5 star is a desired character
             guarantee = True
             number_of_wishes += 1
             
-        else:
+        else:           ##generic result that does not contain 4/5* but still uses a wish
             number_of_wishes += 1
 
-    else:
+    else:       ##guarantee can only be True or False so simply using else should be ok as there is no way i can envision that guarantee is not either
         
-        if result == 1000:
+        if result == 1000:      ##result is a 4* and is guaranteed to be featured. this means that it resets the guarantee
             number_of_wishes += 1
             guarantee = False
             
-        elif result == 2000:
+        elif result == 2000:            ##result is a 5* and is guaranteed to be the desired character
             number_of_wishes += 1
-            exit_loop = True
+            guarantee = False
 
-            with open("number_of_wishes_experimental.txt", mode = "a", encoding = "UTF-8") as file:
+            with open("number_of_wishes_experimental.txt", mode = "a", encoding = "UTF-8") as file:     ##writing to the file
                 file.write(str(number_of_wishes) + "\n")
 
-            number_of_wishes = 0
+            number_of_wishes = 0        ##resetting wish count
+
+        else:               ##wish is not desired and 1 is added to wish count
+            number_of_wishes += 1
                 
